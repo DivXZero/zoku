b0ac50dcefcfcd256d633009b915c636232a7314
DivXZero <silv3rlogic@gmail.com>
2017-02-12 12:21:04 -0500

    haml'fied base

--- lib/zoku/commands/new.rb
+++ lib/zoku/commands/new.rb
@@ -1,4 +1,5 @@
 require 'zoku/common'
+require 'zoku/util'
 
 module Zoku
   module Commands
@@ -8,13 +9,46 @@ module Zoku
       end
 
       def init
-        #{}`rails new #{@target_path} -q --database=postgresql --skip-bundle`
-        FileUtils.mkdir @target_path
-        FileUtils.cd(@target_path)
-        FileUtils.copy_entry "#{base_path}/templates/base", '.'
+        create_project
+        customize_project
         #{}`git init --quiet && git add -A && \
         #git commit -m \"Project setup with Zoku :paw_prints:\" --quiet`
       end
+
+      private
+
+      def create_project
+        FileUtils.mkdir @target_path
+        FileUtils.cd(@target_path)
+        FileUtils.copy_entry "#{base_path}/templates/base", '.'
+      end
+
+      def customize_project
+        replacements = set_replacements
+
+        Dir.glob("./**/*", File::FNM_DOTMATCH) do |file|
+          next if file == '.' || file == '..' || File.directory?(file)
+
+          text = File.read(file)
+
+          replacements.each do |key, value|
+            text.gsub!(key.to_s, value)
+          end
+
+          File.open(file, 'w') { |f| f.puts text }
+        end
+      end
+
+      def set_replacements
+        project_name = @target_path.to_s.dup
+
+        {
+          'zoku_base' => Util.underscore(project_name),
+          'ZokuBase'  => Util.classify(project_name),
+          'VERSION'   => VERSION
+        }
+      end
+
     end
   end
 end
