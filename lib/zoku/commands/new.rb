aa5b2b515ec5bf5cf2d40de9ca69305343b23a53
DivXZero <silv3rlogic@gmail.com>
2017-02-12 10:05:31 -0500

    Small refactor

--- lib/zoku/commands/new.rb
+++ lib/zoku/commands/new.rb
@@ -8,8 +8,8 @@ module Zoku
       end
 
       def init
-        exec "rails new #{@target_path} -q --database=postgresql --skip-bundle && \
-              cd #{@target_path} && \
+        exec "rails new #{@target_path} -q --database=postgresql --skip-bundle"
+        exec "cd #{@target_path} && \
               git init --quiet && git add -A && \
               git commit -m \"Project setup with Zoku :paw_prints:\" --quiet"
       end
